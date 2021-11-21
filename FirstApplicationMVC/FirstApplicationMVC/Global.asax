96039c349bfae6657312215e1cc83cdf08f4beb3
Miguel Henrique <miguelhp373@gmail.com>
2021-11-21 19:52:41 -0300

    Day 01
    » Introduction
    » FirstProject
    » Controllers

---
+++ FirstApplicationMVC/FirstApplicationMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FirstApplicationMVC.MvcApplication" Language="C#" %>
---
+++ FisrtMVCDemo/FisrtMVCDemo/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FisrtMVCDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index(string id)
+        {
+            return "This Id =>" + id + " | Search Text: " + Request.QueryString["search"];
+        }
+    }
+}
